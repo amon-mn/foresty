e721e5eb506fbc5ff0503ed1a304c45b6ce4ff11
amonmn <84486422+amonmn@users.noreply.github.com>
2023-07-20 23:00:06 -0400

    Welcome Page

---
+++ lib/authentication/screens/welcome.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 32,
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 16),
+                Icon(
+                  Icons.forest,
+                  size: 200,
+                ),
+                const SizedBox(height: 68),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Login"),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Cadastro",
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
