7d01e03cedeb257a1720e26b08da27c1a1e9e47e
amonmn <84486422+amonmn@users.noreply.github.com>
2023-07-26 21:05:59 -0400

    Creation of the user registration method

--- lib/authentication/screens/register_page.dart
+++ lib/authentication/screens/register_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../components/my_button.dart';
 import '../../components/my_textfild.dart';
+import '../../components/show_snackbar.dart';
+import '../services/auth_service.dart';
 
 class RegisterScreen extends StatefulWidget {
   RegisterScreen({Key? key}) : super(key: key);
@@ -10,9 +14,120 @@ class RegisterScreen extends StatefulWidget {
 
 class _RegisterScreenState extends State<RegisterScreen> {
   // text editing controllers
-  final nameController = TextEditingController();
-  final emailController = TextEditingController();
-  final cpfController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmationController = TextEditingController();
+  final TextEditingController _cpfController = TextEditingController();
+
+  // variables
+  List<String> statesList = [
+    "Acre",
+    "Alagoas",
+    "Amapá",
+    "Amazonas",
+    "Bahia",
+    "Ceará",
+    "Distrito Federal",
+    "Espírito Santo",
+    "Goiás",
+    "Maranhão",
+    "Mato Grosso",
+    "Mato Grosso do Sul",
+    "Minas Gerais",
+    "Pará",
+    "Paraíba",
+    "Paraná",
+    "Pernambuco",
+    "Piauí",
+    "Rio de Janeiro",
+    "Rio Grande do Norte",
+    "Rio Grande do Sul",
+    "Rondônia",
+    "Roraima",
+    "Santa Catarina",
+    "São Paulo",
+    "Sergipe",
+    "Tocantins",
+  ];
+
+  Map<String, List<String>> citiesByState = {
+    'São Paulo': [
+      'São Paulo',
+      'Campinas',
+      // Adicione mais cidades de São Paulo conforme necessário
+    ],
+    'Rio de Janeiro': [
+      'Rio de Janeiro',
+      'Niterói',
+      // Adicione mais cidades do Rio de Janeiro conforme necessário
+    ],
+    'Amazonas': [
+      'Alvarães',
+      'Amaturá',
+      'Anamã',
+      'Anori',
+      'Apuí',
+      'Atalaia do Norte',
+      'Autazes',
+      'Barcelos',
+      'Barreirinha',
+      'Benjamin Constant',
+      'Beruri',
+      'Boa Vista do Ramos',
+      'Boca do Acre',
+      'Borba',
+      'Caapiranga',
+      'Carauari',
+      'Careiro',
+      'Careiro da Várzea',
+      'Coari',
+      'Codajás',
+      'Eirunepé',
+      'Guajará',
+      'Humaitá',
+      'Ipixuna',
+      'Iranduba',
+      'Itacoatiara',
+      'Itamarati',
+      'Itapiranga',
+      'Japurá',
+      'Juruá',
+      'Jutaí',
+      'Lábrea',
+      'Manacapuru',
+      'Manaquiri',
+      'Manaus',
+      'Manicoré',
+      'Maués',
+      'Nhamundá',
+      'Nova Olinda do Norte',
+      'Novo Airão',
+      'Novo Aripuanã',
+      'Parintins',
+      'Presidente Figueiredo',
+      'Rio Preto da Eva',
+      'Santa Isabel do Rio Negro',
+      'Santo Antônio do Içá',
+      'São Gabriel da Cachoeira',
+      'São Paulo de Olivença',
+      'São Sebastião do Uatumã',
+      'Silves',
+      'Tabatinga',
+      'Tefé',
+      'Tonantins',
+      'Uarini',
+      'Urucará',
+      'Urucurituba',
+    ],
+  };
+
+  String _selectedState = '';
+  String _selectedCity = '';
+
+  final _formKey = GlobalKey<FormState>();
+
+  AuthService authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
@@ -54,44 +169,137 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                const SizedBox(height: 16),
-                MyTextField(
-                  controller: nameController,
-                  hintText: 'Nome completo',
-                  obscureText: false,
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 16),
+                      MyTextField(
+                        controller: _nameController,
+                        hintText: 'Nome completo',
+                        obscureText: false,
+                      ),
+                      const SizedBox(height: 8),
+                      MyTextField(
+                        controller: _emailController,
+                        hintText: 'Digite seu e-mail',
+                        obscureText: false,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "O valor de e-mail deve ser preenchido";
+                          }
+                          if (!value.contains("@") ||
+                              !value.contains(".") ||
+                              value.length < 4) {
+                            return "O e-mail precisa ser válido";
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 8),
+                      MyTextField(
+                        controller: _cpfController,
+                        hintText: 'Digite seu CPF',
+                        obscureText: true,
+                        validator: (value) {
+                          // ... (código de validação do CPF aqui)
+                        },
+                      ),
+                      const SizedBox(height: 8),
+                      DropdownButtonFormField<String>(
+                        value: _selectedState,
+                        items: statesList.map((estado) {
+                          return DropdownMenuItem<String>(
+                            value: estado,
+                            child: Text(estado),
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            _selectedState = value!;
+                            // Ao selecionar um estado, resetar a cidade selecionada
+                            _selectedCity = '';
+                          });
+                        },
+                        decoration: const InputDecoration(
+                          labelText: 'Estado',
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      DropdownButtonFormField<String>(
+                        value: _selectedCity,
+                        items: _selectedState.isEmpty
+                            ? null
+                            : citiesByState[_selectedState]?.map((cidade) {
+                                return DropdownMenuItem<String>(
+                                  value: cidade,
+                                  child: Text(cidade),
+                                );
+                              }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            _selectedCity = value!;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Cidade',
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      MyButton(
+                        onTap: sendRegisterUser,
+                        text_button: 'Entrar',
+                      ),
+                    ],
+                  ),
                 ),
-                const SizedBox(height: 8),
-                MyTextField(
-                  controller: emailController,
-                  hintText: 'Digite seu e-mail',
-                  obscureText: false,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "O valor de e-mail deve ser preenchido";
-                    }
-                    if (!value.contains("@") ||
-                        !value.contains(".") ||
-                        value.length < 4) {
-                      return "O valor do e-mail deve ser válido";
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 8),
-                MyTextField(
-                  controller: cpfController,
-                  hintText: 'Digite seu CPF',
-                  obscureText: true,
-                  validator: (value) {
-                    // ... (código de validação do CPF aqui)
-                  },
-                ),
-                const SizedBox(height: 8),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  // Register Method
+  void sendRegisterUser() {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+    String name = _nameController.text;
+    String cpf = _cpfController.text;
+    String state = _selectedState;
+    String city = _selectedCity;
+
+    if (_formKey.currentState!.validate()) {
+      _createUser(
+          email: email,
+          password: password,
+          name: name,
+          cpf: cpf,
+          state: state,
+          city: city);
+    }
+  }
+
+  _createUser(
+      {required String email,
+      required String password,
+      required String name,
+      required String cpf,
+      required String state,
+      required String city}) {
+    authService
+        .registerUser(
+      password: password,
+      email: email,
+      name: name,
+      state: state,
+      city: city,
+    )
+        .then((String? erro) {
+      if (erro != null) {
+        showSnackBar(context: context, mensagem: erro);
+      }
+    });
   }
 }
